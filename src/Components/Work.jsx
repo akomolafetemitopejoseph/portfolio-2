cbac98a7808494a6c10f0cade7534a212fe1bf03
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-12-11 10:44:25 -0800

    Added changes on my work componenet

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -46,39 +46,80 @@ const Work = () => {
     },
   ];
   return (
-    <div className="px-5 py-5  lg:px-10 lg:pt-10">
+    <div className="px-5 py-5  sm:px-10 sm:pt-10">
       <div>
-        <h1 className="text-center text-2xl lg:text-4xl text-white font-bold capitalize">
+        <h1 className="text-center text-2xl sm:text-4xl text-white font-bold capitalize">
           my projects
         </h1>
       </div>
-      <div className="lg:flex gap-5">
+      <div className="sm:flex gap-5">
         {myProject.map((project, index) => (
-          <div className="border-2 border-green-300 mt-10" key={index}>
+          <div className="border-1 border-green-200 rounded-2xl mt-10" key={index}>
             <div>
               <img
-                className="w-[400px] lg:w-[
-              600px] h-[150px] lg:h-[250px]"
+                className="w-fit sm:w-[
+              600px] h-[150px] sm:h-[250px] rounded-tr-2xl rounded-tl-2xl"
                 src={project.image}
                 alt=""
               />
             </div>
             {/* //NOTE: correction need to be made here ON THE E-COMMANCE PAGE*/}
             <div className="m-5">
-              <div className="text-white font-bold text-xl lg:text-2xl mt-5 lg:mt-10 capitalize">{project.liveDemo}</div>
-              <h1>{project.repo}</h1>
-              <div className="flex">
-                <h1>{project.react}</h1>
-                <h1>{project.tailwind}</h1>
-                <h1>{project.reactScroll}</h1>
-                <h1>{project.antd}</h1>
-                <h1>{project.reactCons}</h1>
-                <h1>{project.aos}</h1>
+              <div className="text-white font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer">
+                {project.liveDemo}
+              </div>
+              <h1 className="text-white font-bold text-xl sm:text-xl capitalize cursor-pointer">
+                {project.repo}
+              </h1>
+              <div className="grid grid-cols-2 gap-2 mt-5">
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl"
+                >
+                  {project.react}
+                </button>
+
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl"
+                >
+                  {project.tailwind}
+                </button>
+
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl"
+                >
+                  {project.reactScroll}
+                </button>
+
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl"
+                >
+                  {project.antd}
+                </button>
+
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl"
+                >
+                  {project.reactCons}
+                </button>
+
+                <button
+                  type="button"
+                  className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl"
+                >
+                  {project.aos}
+                </button>
               </div>
             </div>
           </div>
         ))}
       </div>
+
+      <div></div>
     </div>
   );
 };
