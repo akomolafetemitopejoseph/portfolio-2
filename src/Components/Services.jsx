abf33b706975852866b720eff2de9f015170a257
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2025-12-13 11:12:32 -0800

    update changes

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -65,7 +65,7 @@ const Services = () => {
           your services, I’m here to make your online presence stand out.
         </h1>
       </div>
-      <div className="text-2xl font-bold lg:text-4xl capitalize mt-3 mb-3 lg:mb-5 lg:mt-5 ">
+      <div className="text-2xl font-bold lg:text-4xl capitalize mt-5 mb-3 lg:mb-5 lg:mt-10 ">
         <h1>my services as a frontend developer</h1>
       </div>
 
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -5,17 +5,17 @@ import portfolioimg from "../assets/porfolio.png";
 
 const Work = () => {
   const myProject = [
-    {
-      id: 1,
-      image: ecommanceimg,
-      liveDemo: "live demo",
-      repo: "Repo",
-      react: "React",
-      tailwind: "Tailwind",
-      motion: "framer motion",
-      redux: "React-Redux",
-      reactCons: "React-Icons",
-    },
+    // {
+    //   id: 1,
+    //   image: ecommanceimg,
+    //   liveDemo: "live demo",
+    //   repo: "Repo",
+    //   react: "React",
+    //   tailwind: "Tailwind",
+    //   motion: "framer motion",
+    //   redux: "React-Redux",
+    //   reactCons: "React-Icons",
+    // },
 
     {
       id: 2,
@@ -54,7 +54,10 @@ const Work = () => {
       </div>
       <div className="sm:flex gap-5">
         {myProject.map((project, index) => (
-          <div className="border-1 hover:w-200 border-green-200 rounded-2xl mt-10" key={index}>
+          <div
+            className="border-1 hover:w-200 border-green-200 rounded-2xl mt-10"
+            key={index}
+          >
             <div>
               <img
                 className="w-full sm:w-[
@@ -65,10 +68,10 @@ const Work = () => {
             </div>
             {/* //NOTE: correction need to be made here ON THE E-COMMANCE PAGE*/}
             <div className="m-5">
-              <div className="text-white hover:text-green-100 font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer">
+              <div className="text-white font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
                 {project.liveDemo}
               </div>
-              <h1 className="text-white hover:text-green-100 mt-5 font-bold text-xl sm:text-xl capitalize cursor-pointer">
+              <h1 className="text-white font-bold text-xl sm:text-2xl mt-3 sm:mt-5 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
                 {project.repo}
               </h1>
               <div className="grid grid-cols-2 gap-2 mt-5">
@@ -117,9 +120,46 @@ const Work = () => {
             </div>
           </div>
         ))}
-      </div>
 
-      <div></div>
+        <div className="border-1 hover:w-200 border-green-200 rounded-2xl mt-10">
+          <div>
+            <img
+              className="w-full sm:w-[
+              600px] h-[150px] sm:h-[250px] rounded-tr-2xl rounded-tl-2xl"
+              src={ecommanceimg}
+              alt=""
+            />
+          </div>
+
+          <div className="m-5">
+            <p className="text-white font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
+              live demo
+            </p>
+
+            <p className="text-white font-bold text-xl sm:text-2xl mt-3 sm:mt-5 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
+              repo
+            </p>
+
+            <div className="grid grid-cols-2 gap-2 mt-5">
+              <p className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl">
+                react
+              </p>
+              <p className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl">
+                tailwind
+              </p>
+              <p className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl">
+                framer motion
+              </p>
+              <p className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl">
+                React-Redux
+              </p>
+              <p className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl">
+                React-Icons
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
