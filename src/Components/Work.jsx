9957c2974ac39cc4a1f5e5cb15a687a6b09b4ec5
akomolafetemitopejoseph <134941960+akomolafetemitopejoseph@users.noreply.github.com>
2026-01-07 13:26:05 -0800

    Added link work page

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -8,8 +8,8 @@ const Work = () => {
     {
       id: 2,
       image: imgtool,
-      liveDemo: "live demo",
-      repo: "Repo",
+      liveDemo: "https://akomolafetemitopejoseph.github.io/landingpage/",
+      repo: "https://github.com/akomolafetemitopejoseph",
       react: "React",
       tailwind: "Tailwind",
       motion: "framer motion",
@@ -22,8 +22,8 @@ const Work = () => {
     {
       id: 3,
       image: portfolioimg,
-      liveDemo: "live demo",
-      repo: "Repo",
+      liveDemo: "https://akomolafetemitopejoseph.github.io/myprofile/",
+      repo: "https://github.com/akomolafetemitopejoseph",
       react: "React",
       tailwind: "Tailwind",
       motion: "framer motion",
@@ -62,14 +62,18 @@ const Work = () => {
                 alt=""
               />
             </div>
-            {/* //NOTE: correction need to be made here ON THE E-COMMANCE PAGE*/}
+
             <div className="m-5">
-              <div className="text-white font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
-                {project.liveDemo}
-              </div>
-              <h1 className="text-white font-bold text-xl sm:text-2xl mt-3 sm:mt-5 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
-                {project.repo}
-              </h1>
+              <a href={project.liveDemo}>
+                <div className="text-white font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
+                  live Demo
+                </div>
+              </a>
+              <a href={project.repo}>
+                <h1 className="text-white font-bold text-xl sm:text-2xl mt-3 sm:mt-5 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
+                  repo
+                </h1>
+              </a>
               <div className="grid grid-cols-2 gap-2 mt-5">
                 <button
                   type="button"
@@ -121,13 +125,16 @@ const Work = () => {
           </div>
 
           <div className="m-5">
-            <p className="text-white font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
-              live demo
-            </p>
-
-            <p className="text-white font-bold text-xl sm:text-2xl mt-3 sm:mt-5 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
-              repo
-            </p>
+            <a href="https://akomolafetemitopejoseph.github.io/e-commance/">
+              <p className="text-white font-bold text-xl sm:text-2xl mt-5 sm:mt-10 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
+                live demo
+              </p>
+            </a>
+            <a href="https://github.com/akomolafetemitopejoseph">
+              <p className="text-white font-bold text-xl sm:text-2xl mt-3 sm:mt-5 capitalize cursor-pointer  bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 rounded-base text-l px-2 py-2 text-center leading-5 hover:text-green-900 rounded-xl w-fit">
+                repo
+              </p>
+            </a>
 
             <div className="grid grid-cols-2 gap-2 mt-5">
               <p className="text-white bg-gradient-to-r from-gray-400 via-gray-500 to-gray-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-sm shadow-green-500/50 dark:shadow-sm dark:shadow-green-800/80 font-medium rounded-base text-l capitalize px-2 py-2 text-center leading-5 cursor-pointer hover:text-green-900 rounded-xl">
